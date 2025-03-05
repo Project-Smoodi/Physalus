2cff1613b92ca5fd05e8f77604225dcdc362f306
Daybreak312 <ty82afg12@gmail.com>
2025-03-05 20:04:33 +0900

    ADD :: 작동 불가 Physalus 추가

---
+++ src/main/java/org/smoodi/physalus/engine/Physalus.java
@@ -0,0 +1,78 @@
+package org.smoodi.physalus.engine;
+
+import org.smoodi.physalus.Adapter;
+import org.smoodi.physalus.Port;
+import org.smoodi.physalus.exchange.Request;
+
+import java.net.Socket;
+import java.util.List;
+
+public class Physalus
+        implements Engine, ListeningEngine, Adapted, Ported {
+
+    private static Physalus engine;
+
+    private final HttpServer httpServer = new HttpServer();
+
+    private final AdapterManager adapterModule = new AdapterManager() {
+    };
+
+    @Override
+    public void startEngine() {
+
+    }
+
+    @Override
+    public void stopEngine() {
+
+    }
+
+    @Override
+    public void doService(Socket socket, List<String> tags) {
+
+    }
+
+    public void mainController(Request request, List<String> tags) {
+
+    }
+
+    @Override
+    public boolean addAdapter(Adapter adapter) {
+        return adapterModule.addAdapter(adapter);
+    }
+
+    @Override
+    public boolean removeAdapter(Adapter adapter) {
+        return adapterModule.removeAdapter(adapter);
+    }
+
+    @Override
+    public boolean removeAdapter(String tag) {
+        return adapterModule.removeAdapter(tag);
+    }
+
+    @Override
+    public boolean addPort(Port port) {
+        return httpServer.addPort(port);
+    }
+
+    @Override
+    public boolean addPort(int port) {
+        return httpServer.addPort(port);
+    }
+
+    @Override
+    public boolean removePort(Port port) {
+        return httpServer.removePort(port);
+    }
+
+    @Override
+    public boolean removePort(int port) {
+        return httpServer.removePort(port);
+    }
+
+    @Override
+    public boolean removePort(String tag) {
+        return httpServer.removePort(tag);
+    }
+}
