512587445999086ef2968dfed58f6f60d63f7697
Daybreak312 <ty82afg12@gmail.com>
2025-03-19 17:08:25 +0900

    FEAT :: IOSocket HTTP Response 작성기 추가

---
+++ src/main/java/org/smoodi/physalus/transfer/http/IOStreamHttpResponseSender.java
@@ -0,0 +1,28 @@
+package org.smoodi.physalus.transfer.http;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.smoodi.physalus.transfer.socket.IOStreamSocket;
+
+import java.io.IOException;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class IOStreamHttpResponseSender {
+
+    public static void sendInternalServerError(final IOStreamSocket socket) {
+        if (socket.isOutputShutdown()) {
+            return;
+        }
+
+        try {
+            socket.getOutput().write(
+                    ResponseSerializer.serialize(
+                            new SocketBasedHttpExchange.Response(
+                                    socket.toNative().getRemoteSocketAddress().toString()
+                            )
+                    )
+            );
+        } catch (IOException ignore) {
+        }
+    }
+}
