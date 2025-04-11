10fb8d8fd2994943bca4c75f41c163e4501f26d7
Daybreak312 <ty82afg12@gmail.com>
2025-04-11 21:49:47 +0900

    FEAT :: AdapterManager.execute 구현 및 NotNull 처리

--- src/main/java/org/smoodi/physalus/engine/adapter/AdapterContext.java
+++ src/main/java/org/smoodi/physalus/engine/adapter/AdapterContext.java
@@ -1,10 +1,12 @@
 package org.smoodi.physalus.engine.adapter;
 
+import org.smoodi.annotation.NotNull;
+
 public interface AdapterContext extends AdapterManager {
 
-    boolean addAdapter(Adapter adapter);
+    boolean addAdapter(@NotNull Adapter adapter);
 
-    boolean removeAdapter(Adapter adapter);
+    boolean removeAdapter(@NotNull Adapter adapter);
 
-    boolean removeAdapter(String tag);
+    boolean removeAdapter(@NotNull String tag);
 }
--- src/main/java/org/smoodi/physalus/engine/adapter/PhysalusAdapterManager.java
+++ src/main/java/org/smoodi/physalus/engine/adapter/PhysalusAdapterManager.java
@@ -1,29 +1,40 @@
 package org.smoodi.physalus.engine.adapter;
 
+import org.smoodi.annotation.NotNull;
 import org.smoodi.physalus.transfer.Exchange;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class PhysalusAdapterManager implements AdapterContext {
 
-    private final Set<Adapter> adapters = new HashSet<>();
+    private final Set<Adapter> adapters = new LinkedHashSet<>();
 
-    public void execute(Exchange exchange, String tag) {
+    public void execute(@NotNull Exchange exchange, @NotNull String tag) {
+        assert exchange != null;
+        assert tag != null;
+
+        for (Adapter adapter : adapters) {
+            if (Objects.equals(adapter.getTag(), tag)) {
+                adapter.execute(exchange);
+            }
+        }
     }
 
     @Override
-    public boolean addAdapter(Adapter adapter) {
+    public boolean addAdapter(@NotNull Adapter adapter) {
+        assert adapter != null;
         return adapters.add(adapter);
     }
 
     @Override
-    public boolean removeAdapter(Adapter adapter) {
+    public boolean removeAdapter(@NotNull Adapter adapter) {
+        assert adapter != null;
         return adapters.remove(adapter);
     }
 
     @Override
     public boolean removeAdapter(String tag) {
+        assert tag != null;
         return adapters.removeIf(it -> it.getTag().contains(tag));
     }
 }
