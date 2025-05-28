b8d2bd4bf2742e1dca70e4da76ce77e885109749
Daybreak312 <ty82afg12@gmail.com>
2025-05-28 14:18:23 +0900

    SNAPSHOT :: v0.0.3-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,7 +70,7 @@ publishing {
 
             groupId = "org.smoodi.framework"
             artifactId = "physalus"
-            version = "0.0.2-SNAPSHOT"
+            version = "0.0.3-SNAPSHOT"
 
             pom {
                 name.set("Physalus The Engine")
